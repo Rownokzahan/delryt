9c47c759e14e4e362a6e49e3742867bc6e0318ba
Rownok Zahan <rownokzahan9@gmail.com>
2025-04-08 17:47:30 +0600

    Add header for Popular Cuisines

--- src/app/(main)/home/Cuisines/index.tsx
+++ src/app/(main)/home/Cuisines/index.tsx
@@ -4,11 +4,12 @@ const Cuisines = () => {
   return (
     <div className="bg-primary">
       <div className="ui-container py-12">
-
+        <h3 className="mb-4 md:mb-10 text-uiWhite text-2xl md:text-3xl font-semibold text-center md:text-left">
+          Popular Cuisines
+        </h3>
 
         {/* Carousel */}
         <CuisinesCarousel />
-
       </div>
     </div>
   );
