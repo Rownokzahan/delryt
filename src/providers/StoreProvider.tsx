66b1be2b2cad1bd39d85122d289ef9767ead554e
Rownok Zahan <rownokzahan9@gmail.com>
2025-04-17 12:21:11 +0600

    Refactor StoreProvider and branchApi to initialize current branch using fetched data from localStorage

--- src/providers/StoreProvider.tsx
+++ src/providers/StoreProvider.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import { store } from "@/store/store";
-import { ReactNode, useEffect } from "react";
+import { ReactNode } from "react";
 import { Provider } from "react-redux";
-import { useDispatch } from "react-redux";
 import LoadingPage from "@/components/ui/LoadingPage";
 import { useGetBranchListQuery } from "@/store/branch/branchApi";
-import { initializeCurrentBranch } from "@/store/branch/branchSlice";
 
 const StoreProvider = ({ children }: { children: ReactNode }) => {
   return (
@@ -17,23 +15,8 @@ const StoreProvider = ({ children }: { children: ReactNode }) => {
 };
 
 const StoreInitializerWrapper = ({ children }: { children: ReactNode }) => {
-  const dispatch = useDispatch();
-
-  const { data: branches = [], isLoading: isBranchesLoading } =
-    useGetBranchListQuery();
-
-  // Initialize the current branch once the branches is loaded
-  useEffect(() => {
-    if (isBranchesLoading) {
-      return;
-    }
-
-    const branchId = localStorage.getItem("branchId");
-    const branch =
-      branches.find((branch) => branch.id === Number(branchId)) ?? null;
-
-    dispatch(initializeCurrentBranch(branch));
-  }, [dispatch, branches, isBranchesLoading]);
+  // Wait for the branches list to load and initialize the current branch before rendering the app
+  const { isLoading: isBranchesLoading } = useGetBranchListQuery();
 
   if (isBranchesLoading) {
     return <LoadingPage />;
--- src/store/branch/branchApi.ts
+++ src/store/branch/branchApi.ts
@@ -1,6 +1,7 @@
 import { API_BASE_URL } from "@/config/apiConfig";
 import { Branch } from "@/types";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { initializeCurrentBranch } from "./branchSlice";
 
 export const branchApi = createApi({
   reducerPath: "branchApi",
@@ -8,6 +9,23 @@ export const branchApi = createApi({
   endpoints: (builder) => ({
     getBranchList: builder.query<Branch[], void>({
       query: () => "/branch/list",
+
+      // Use the fetched branches list to identify the current branch using the saved branchId from localStorage,
+      // then set this branch as the current branch in the Redux store.
+      onQueryStarted: async (_, { queryFulfilled, dispatch }) => {
+        try {
+          const { data: branches } = await queryFulfilled;
+
+          const branchId = localStorage.getItem("branchId");
+
+          const branch =
+            branches.find((branch) => branch.id === Number(branchId)) ?? null;
+
+          dispatch(initializeCurrentBranch(branch)); // Set the current branch in Redux state
+        } catch (error) {
+          console.error("Error fetching branch list:", error);
+        }
+      },
     }),
   }),
 });
