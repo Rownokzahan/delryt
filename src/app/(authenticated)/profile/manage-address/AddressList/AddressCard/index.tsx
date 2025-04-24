68ed6f084a2466e63ced1d757f11ec98a157ba0c
Rownok Zahan <rownokzahan9@gmail.com>
2025-04-24 16:46:39 +0600

    Add delete address functionality with confirmation and loading state in AddressCard

--- src/app/(authenticated)/profile/manage-address/AddressList/AddressCard/index.tsx
+++ src/app/(authenticated)/profile/manage-address/AddressList/AddressCard/index.tsx
@@ -1,5 +1,8 @@
 import { MdEdit, MdDelete } from "react-icons/md";
 import { Address } from "@/types";
+import { useDeleteAddressMutation } from "@/store/features/address/adressApi";
+import toast from "react-hot-toast";
+import { TbInnerShadowTopRightFilled } from "react-icons/tb";
 
 interface AddressCardProps {
   addressItem: Address;
@@ -14,6 +17,18 @@ const AddressCard = ({ addressItem }: AddressCardProps) => {
     is_default,
   } = addressItem;
 
+  const [deleteAddress, { isLoading: isDeleting }] = useDeleteAddressMutation();
+
+  const handleDelete = async () => {
+    try {
+      await deleteAddress(addressItem.id).unwrap();
+      toast.success("Address deleted successfully!");
+    } catch (error) {
+      console.log("Failed to delete address", error);
+      toast.error("Failed to delete address!");
+    }
+  };
+
   return (
     <div className="rounded-lg shadow-sm">
       <div className="px-4 pt-3 pb-2 rounded-t-lg bg-primary/5 flex items-center justify-between">
@@ -23,9 +38,16 @@ const AddressCard = ({ addressItem }: AddressCardProps) => {
         </h4>
 
         <div className="flex justify-end gap-4  text-sm font-medium underline">
-          <button className="flex items-center gap-1 text-red-600">
-            <MdDelete className="text-lg" />
-          </button>
+          <div className="text-red-600 text-lg">
+            {isDeleting ? (
+              <TbInnerShadowTopRightFilled className="animate-spin flex" />
+            ) : (
+              <button onClick={handleDelete} className="flex">
+                <MdDelete />
+              </button>
+            )}
+          </div>
+
           <button className="flex items-center gap-1">
             <MdEdit className="text-base" />
           </button>
--- src/store/features/address/adressApi.ts
+++ src/store/features/address/adressApi.ts
@@ -1,15 +1,17 @@
 import authorizationBaseQuery from "@/store/utils/authorizationBaseQuery";
-import { Address } from "@/types";
+import { Address, Id } from "@/types";
 import { createApi } from "@reduxjs/toolkit/query/react";
 
 export const addressApi = createApi({
   reducerPath: "addressApi",
   baseQuery: authorizationBaseQuery,
+  tagTypes: ["AddressList"],
   endpoints: (builder) => ({
     getAddressList: builder.query<Address[], void>({
       query: () => ({
         url: "customer/address/list",
       }),
+      providesTags: ["AddressList"],
     }),
 
     createAddress: builder.mutation<void, Address>({
@@ -18,8 +20,21 @@ export const addressApi = createApi({
         method: "POST",
         body: newAddress,
       }),
+      invalidatesTags: ["AddressList"],
+    }),
+
+    deleteAddress: builder.mutation<void, Id>({
+      query: (addressId: Id) => ({
+        url: `customer/address/delete?address_id=${addressId}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["AddressList"],
     }),
   }),
 });
 
-export const { useGetAddressListQuery, useCreateAddressMutation } = addressApi;
+export const {
+  useGetAddressListQuery,
+  useCreateAddressMutation,
+  useDeleteAddressMutation,
+} = addressApi;
