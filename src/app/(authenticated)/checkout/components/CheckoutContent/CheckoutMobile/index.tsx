98e0e8c8be6fcb21f4b5d9c70af01d799f9d9be3
Rownok Zahan <rownokzahan9@gmail.com>
2025-05-07 18:52:23 +0600

    Refactor CheckoutMobile component: replace CheckoutDetails with CartItems, Coupons, BillSummary, and OrderNoteForm for improved mobile checkout experience

--- src/app/(authenticated)/checkout/components/CheckoutContent/CheckoutMobile/index.tsx
+++ src/app/(authenticated)/checkout/components/CheckoutContent/CheckoutMobile/index.tsx
@@ -1,12 +1,19 @@
-import ChangeDeliveryTime from "../../ChangeDeliveryTime";
-import CheckoutDetails from "../../CheckoutDetails";
+import OrderNoteForm from "@/forms/OrderNoteForm";
+import BillSummary from "../../BillSummary";
+import CartItems from "../../CartItems";
+import Coupons from "../../Coupons";
 import FixedBottomBar from "./FixedBottomBar";
 
 const CheckoutMobile = () => {
   return (
     <div className="md:hidden ui-container pt-3 pb-20 space-y-4">
-      <ChangeDeliveryTime />
-      <CheckoutDetails />
+      <div className="bg-uiWhite rounded-lg">
+        Cart
+        <CartItems />
+      </div>
+      <Coupons />
+      <BillSummary />
+      <OrderNoteForm />
       <FixedBottomBar />
     </div>
   );
