9a3f2709851dbf412ef3643323a44ba23577a739
Rownok Zahan <rownokzahan9@gmail.com>
2025-05-18 16:41:22 +0600

    Add loading skeleton for CategoryContent: implement placeholders for banner, filters, and product list

--- src/app/(main)/categories/[categoryId]/CategoryContentSkeleton.tsx
+++ src/app/(main)/categories/[categoryId]/CategoryContentSkeleton.tsx
@@ -1,8 +1,19 @@
+import CategoryProductListSkeleton from "./CategoryContent/CategoryProductList/CategoryProductListSkeleton";
+
 const CategoryContentSkeleton = () => {
   return (
-    <div>
-      <h1>Loading....</h1>
-    </div>
+    <main className="bg-gray-100">
+      {/* Banner placeholder */}
+      <section className="mt-2 h-48 bg-gray-300 animate-pulse" />
+
+      {/* Filters placeholder */}
+      <section className="h-27 md:h-14 bg-gray-200 animate-pulse" />
+
+      {/* Product list placeholder */}
+      <section className="min-h-[60dvh] ui-container pb-8 pt-4 md:pt-8 relative">
+        <CategoryProductListSkeleton />
+      </section>
+    </main>
   );
 };
 
