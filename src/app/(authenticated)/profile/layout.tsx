e7e8103320065eb8bc212e43b09251b3b62ada15
Rownok Zahan <rownokzahan9@gmail.com>
2025-06-01 17:55:29 +0600

    Refactor ProfileLayout: increase max width for improved layout consistency

--- src/app/(authenticated)/profile/layout.tsx
+++ src/app/(authenticated)/profile/layout.tsx
@@ -13,7 +13,7 @@ const ProfileLayout = ({ children }: ProfileLayoutProps) => {
       <div className="hidden sm:block bg-uiBlack/10">
         <NavbarDesktop />
 
-        <div className="py-8 px-4 max-w-6xl mx-auto">
+        <div className="py-8 px-4 max-w-7xl mx-auto">
           <div className="h-[calc(100dvh-var(--h-nav-desktop)-64px)] bg-uiWhite grid grid-cols-13">
             <ProfileSidebar />
 
